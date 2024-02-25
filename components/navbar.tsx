82577dbf0de7569b89dd8a61f1ec64ba23462784
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2024-02-25 17:19:45 +0100

    added Navbar component

---
+++ components/navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Menu } from "lucide-react";
+
+// ui components
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center p-4">
+      <Button variant="ghost" size="icon" className="md:hidden">
+        <Menu />
+      </Button>
+    </div>
+  );
+};
+
+export default Navbar;
