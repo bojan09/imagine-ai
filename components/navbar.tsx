ab1a2f234920e01e7522c37d945963ea5b2c6eaa
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2024-02-25 17:57:34 +0100

    added MobileSidebar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,12 +7,13 @@ import { UserButton } from "@clerk/nextjs";
 // ui components
 import { Button } from "./ui/button";
 
+// components
+import MobileSidebar from "./mobile-sidebar";
+
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
-      <Button variant="ghost" size="icon" className="md:hidden">
-        <Menu />
-      </Button>
+      <MobileSidebar />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
