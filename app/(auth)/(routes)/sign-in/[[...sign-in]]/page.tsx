3a410a31524a0ad1af6c892dce631e645eb0bf64
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2024-02-28 15:12:43 +0100

    minor bug fix with sign-up page

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,22 +1,9 @@
-import React from "react";
-import Link from "next/link";
-
-// ui components
-import { Button } from "@/components/ui/button";
-
-// clerk
 import { SignIn } from "@clerk/nextjs";
 
 const SignInPage = () => {
   return (
     <div>
       <SignIn />
-
-      <div className="mt-7 grid place-items-center">
-        <Link href="/">
-          <Button variant="secondary">Homepage</Button>
-        </Link>
-      </div>
     </div>
   );
 };
