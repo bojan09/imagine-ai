7aa5f80cf475aa742f9127f2425bed877b716a58
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2024-02-28 14:00:07 +0100

    added LandingLayout, and LandingNavbar

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,25 +1,13 @@
 import React from "react";
 import Link from "next/link";
 
-// ui components
-import { Button } from "@/components/ui/button";
+// components
+import { LandingNavbar } from "@/components/landing-navbar";
 
 const LandingPage = () => {
   return (
-    <div className="grid gap-5 ml-4">
-      <p>LandingPage (Unprotected)</p>
-
-      <Link href="/sign-in">
-        <Button>Login</Button>
-      </Link>
-
-      <Link href="/sign-up">
-        <Button>Register</Button>
-      </Link>
-
-      <Link href="/dashboard">
-        <Button>Dashboard</Button>
-      </Link>
+    <div className="h-full">
+      <LandingNavbar />
     </div>
   );
 };
